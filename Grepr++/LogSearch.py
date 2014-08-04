e69469e59bacc21bb9dfacdf9a5a499459199010
Don Johnson <dj@codetestcode.io>
2014-08-04 07:08:03 -0700

    Reworked Regex
    took repeat code and  consolidated into a prefix and suffix

--- Grepr++/LogSearch.py
+++ Grepr++/LogSearch.py
@@ -8,6 +8,8 @@ class LogSearch:
 	def __init__(self): pass
 
 	def search(self, file_path,search_type):
-
+		reg_ex_prefix = '((?:2|1)\\d{3}(?:-|\\/)(?:(?:0[1-9])|(?:1[0-2]))(?:-|\\/)(?:(?:0[1-9])|(?:[1-2][0-9])|(?:3[0-1]))(?:T|\\s)(?:(?:[0-1][0-9])|(?:2[0-3])):(?:[0-5][0-9]):(?:[0-5][0-9])(,)(\\d\\d\\d)( )(\\[.*?\\])( )('
+		reg_ex_suffix = '))'
 		self.file_path = file_path
 		self.search_type = search_type
+
